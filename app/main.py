18b6fcbe55368d3a6c4a1fc6ca43e104b5e283d9
Yaroslav Zhuravlov <zyzy92187@gmail.com>
2025-02-11 16:11:29 +0200

    fix: simplify CarWashStation serve_cars method. add round for income in this method

--- app/main.py
+++ app/main.py
@@ -16,17 +16,14 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> float:
-        serving_cars = [car for car in cars if
-                        car.clean_mark < self.clean_power]
+        income = 0
 
-        income = sum(
-            [self.calculate_washing_price(car) for car in serving_cars]
-        )
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
 
-        for car in serving_cars:
-            self.wash_single_car(car)
-
-        return income
+        return round(income, 2)
 
     def calculate_washing_price(self, car: Car) -> float:
         amount_of_work = self.clean_power - car.clean_mark
